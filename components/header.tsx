349eee6ee4603bbadfb805db77d56316265fd668
zhouzhou918@163.com <zhouzhou918@163.com>
2023-06-21 23:58:23 -0500

    feat: header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = ()  => {
+
   return (
-    <div id="app-container" className="flex">
-      <nav className="flex justify-center relative top-1-4 pl3 pr4">
-        <img src="../assets/logo.svg" alt="Logo" />
+    <div id="app-container">
+      <nav className="flex justify-between items-center top-1-4 mx-12">
+        <Image src="/logo.svg" alt="Logo" width="256" height="256"/>
+        <Link className="navbar-brand" href="/">
+            Home
+          </Link>
       </nav>
     </div>
   )

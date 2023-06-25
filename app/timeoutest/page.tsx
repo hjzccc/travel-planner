632b077b4b38b31149438dfb4ecc0505f4592ed6
h21565897 <h21565897@gmail.com>
2023-06-25 01:23:39 -0700

    update timeout test

--- app/timeoutest/page.tsx
+++ app/timeoutest/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 import React from "react";
 import useSWR from "swr";
+import fetcher from "@/common/swrFetcher";
 const timeoutfetcher = async () => {
   await new Promise((resolve) => setTimeout(resolve, 15000));
   return "done";
 };
 function Page() {
-  const { data, isLoading, error } = useSWR("/api", timeoutfetcher);
+  const { data, isLoading, error } = useSWR("/api/chat/chatForPlan", (url) =>
+    fetcher(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        destination: "New York",
+        features: ["museum", "park", "restaurant"],
+        days: 3,
+        tripLevel: "normal",
+      }),
+    })
+  );
   if (isLoading) return <div>loading...</div>;
   if (error) return <div>error</div>;
   return <div>Page</div>;
